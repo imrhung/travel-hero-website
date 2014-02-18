b7b87bff9d2cf4957831648e2158e4a88add34c1
nguyenvanhungbkit@gmail.com <nguyenvanhungbkit@gmail.com@31c8e538-c165-4d65-4c91-bab099a6a01f>
2014-02-18 02:51:36 +0000

    Add quest list page

--- libs/View.php
+++ libs/View.php
@@ -1,13 +1,17 @@
 <?php
+
 class View {
+
     function __construct() {
+        
     }
+
     public function render($name, $noInclude = false) {
-        if($noInclude == true) {
+        if ($noInclude == true) {
+            require 'views/' . $name . '.php';
+        } else {
             require 'views/' . $name . '.php';
         }
-        else {         
-            require 'views/' . $name . '.php';            
-        }
     }
+
 }
