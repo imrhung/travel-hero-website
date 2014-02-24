3cb2921d3e8b892a1ba84b2c547ba2d404b3b223
nguyenvanhungbkit@gmail.com <nguyenvanhungbkit@gmail.com@31c8e538-c165-4d65-4c91-bab099a6a01f>
2014-02-24 08:35:58 +0000

    Port web to CodeIgniter

--- testpages/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Config
-require 'config.php';
-
-// Also spl_autoload_register (Take a mlook at it if you like)
-function __autoload($class) {
-    require LIBS . $class .".php";
-}
-$app = new Bootstrap();
